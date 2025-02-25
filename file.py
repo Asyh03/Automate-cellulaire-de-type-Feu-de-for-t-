6a8412186ffdf468f6a4e54cfe05fff9a26761d3
Asyh03 <165148214+Asyh03@users.noreply.github.com>
2025-02-25 14:11:53 +0100

    Initialisation des variables

--- file.py
+++ file.py
@@ -1,1 +1,13 @@
+from tkinter import *
+import random
 
+# Paramètres de la grille
+hauteur = int(input("Entrez la hauteur de la grille : "))
+largeur = int(input("Entrez la largeureur de la grille : "))  # Taille de la grille
+cote = int(input("Entrez la taille d'une cellule en pixel : "))
+
+# États des cellules
+VIDE = 0
+ARBRE = 1
+FEU = 2
+couleurs = {VIDE: "white", ARBRE: "green", FEU: "red"}
